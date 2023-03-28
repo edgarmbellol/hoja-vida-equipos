20b226a18c1393d02e31a8f7769e748b224b57bc
Edgar Mauricio Bello Lamprea <35710049+edgarmbellol@users.noreply.github.com>
2023-03-28 15:01:19 -0500

    Programa final
    Programa realiza todo el proceso de buscar en los archivos csv lo necesario para la hoja de vida del computador y lo pone en una hoja de excel con los campos requeridos.
    Falta realizar un ejecutable

--- PROGRAMA/buscarcsv.py
+++ PROGRAMA/buscarcsv.py
@@ -1,5 +1,12 @@
 import csv
 import openpyxl # TRABAJA CON HOJAS DE EXCEL
+import os
+
+def lista_archivos():
+    carpeta = "./archivoscsv"
+    # carpeta = "C:\\Users\\Area Sistemas HDS\\Desktop\\hoja-vida-equipos-main\\hoja-vida-equipos-main\\PROGRAMA\\archivoscsv"
+    archivos = os.listdir(carpeta)
+    return archivos
 
 def buscar_palabra(ruta,dicc):
     lista =[] # lista donde se almacenaran las busquedas
@@ -27,18 +34,28 @@ def buscar_palabra(ruta,dicc):
                             break
     return lista
 
-def escribir_excel(datos,camposExcel,nomArchivo):
+def escribir_excel(datos,camposExcel,nomArchivo,workbook):
     # CODIGO PARA PONER EN HOJA DE EXCEL
 
-    workbook = openpyxl.load_workbook('formato.xlsx')
     hoja = workbook['HOJA DE VIDA DE EQUIPOS']
-
     for i in range(0,len(datos),1):
         #PONER EN LA HOJA DE EXCEL LOS DATOS CORRRESPONDIENTES
         hoja[camposExcel[i]] = datos[i]
 
     workbook.save(nomArchivo)
+    workbook.close()
     return
+
+def obtNombre(ruta):
+    nombre = ""
+    # TOMA EL NOMBRE DEL ARCHIVO SIN EXTENCIONES
+    ruta = ruta[:-4]
+    for i in reversed(ruta):
+        if i == "/":
+            break
+        else:
+            nombre = i + nombre
+    return nombre
 
 # DICCIONARIO VACIO CON  VALORES QUE SE BUSCARAN EN LA HOJA CSV
 # EL PRIMER VALOR DE CADA LISTA HACE REFERENCIA A LA SECCION DEL ARCHIVO
@@ -51,13 +68,13 @@ disco = ["Unidades de disco","Modelo de unidad:","Número de serie de la unidad:
 cd = ["DVD","Modelo de unidad:","Número de serie:"]
 monitor = ["Monitor","Nombre del monitor:","Nombre del monitor (del fabricante):","Número de serie:"]
 red = ["Ethernet","Dirección MAC:"]
-sistema = ["Nombre del ordenador:","Sistema operativo:"]
+sistema = ["Nombre del ordenador:","Sistema operativo:","Nombre de la marca de la computadora:"]
 
 # CAMPOS DE EXCEL NECESARIOS PARA COLOCAR DATOS
 # contiene el nombre de la celda donde debe ser colocados los datos, la lista debe tener el mismo tamaño
 # que la lista que arroja el programa
-camposequipo =["Y11"]
-camposcpu =["M15","Z15"]
+camposequipo =["Y11",]
+camposcpu =["G15","Z15"]
 camposprocesador = ["G17","T17","Z17"]
 camposram1 = ["G19","T19","Z19"]
 camposram2 = ["G21","T21","Z21"]
@@ -65,34 +82,38 @@ camposdisco = ["G23","Z23","T23"]
 camposcd = ["G24","Z24"]
 camposmonitor = ["G26","Z26","Z27"]
 camposred = ["Y36"]
-campossistema = ["K42"]
+campossistema = ["K42","M15"]
 
-ruta = "C:\\Users\\Area Sistemas HDS\\Desktop\\hoja-vida-equipos-main\\hoja-vida-equipos-main\\PROGRAMA\\archivoscsv\\COMUNICACIONES2.CSV"
-# ruta = "archivoscsv\COMUNICACIONES2.CSV"
-# TOMA EL NOMBRE DEL ARCHIVO SIN EXTENCIONES
-nomArchivo = ruta[:-4]
-nomArchivo = nomArchivo + ".xlsx"
-lista_final = buscar_palabra(ruta,equipo)
-escribir_excel(lista_final,camposequipo,nomArchivo) # Impresion en archivo de excel
-lista_final = buscar_palabra(ruta,cpu)
-escribir_excel(lista_final,camposcpu,nomArchivo) # Impresion en archivo de excel
-lista_final = buscar_palabra(ruta,procesador)
-escribir_excel(lista_final,camposprocesador,nomArchivo) # Impresion en archivo de excel
-lista_final = buscar_palabra(ruta,ram1)
-escribir_excel(lista_final,camposram1,nomArchivo) # Impresion en archivo de excel
-lista_final = buscar_palabra(ruta,ram2)
-escribir_excel(lista_final,camposram2,nomArchivo) # Impresion en archivo de excel
-lista_final = buscar_palabra(ruta,disco)
-escribir_excel(lista_final,camposdisco,nomArchivo) # Impresion en archivo de excel
-lista_final = buscar_palabra(ruta,cd)
-escribir_excel(lista_final,camposcd,nomArchivo) # Impresion en archivo de excel
-lista_final = buscar_palabra(ruta,monitor)
-escribir_excel(lista_final,camposmonitor,nomArchivo) # Impresion en archivo de excel
-lista_final = buscar_palabra(ruta,red)
-escribir_excel(lista_final,camposred,nomArchivo) # Impresion en archivo de excel
-lista_final = buscar_palabra(ruta,sistema) 
-escribir_excel(lista_final,campossistema,nomArchivo) # Impresion en archivo de excel
-print("PROCESO FINALIZADO")
+# RUTA DONDE SE ALMACENA EL ARCHIVO A BUSCAR
+archivos = lista_archivos()
+workbook = openpyxl.load_workbook('formato.xlsx')
+for i in archivos:
+    # print("./archivoscsv/"+i)
+    ruta = "./archivoscsv/"+i
+    # ruta = "C:\\Users\\Area Sistemas HDS\\Desktop\\hoja-vida-equipos-main\\hoja-vida-equipos-main\\PROGRAMA\\archivoscsv\\" + i
+    nomArchivo = obtNombre(ruta)
+    nomArchivo = nomArchivo + ".xlsx"
+    lista_final = buscar_palabra(ruta,equipo)
+    escribir_excel(lista_final,camposequipo,nomArchivo,workbook) # Impresion en archivo de excel
+    lista_final = buscar_palabra(ruta,cpu)
+    escribir_excel(lista_final,camposcpu,nomArchivo,workbook) # Impresion en archivo de excel
+    lista_final = buscar_palabra(ruta,procesador)
+    escribir_excel(lista_final,camposprocesador,nomArchivo,workbook) # Impresion en archivo de excel
+    lista_final = buscar_palabra(ruta,ram1)
+    escribir_excel(lista_final,camposram1,nomArchivo,workbook) # Impresion en archivo de excel
+    lista_final = buscar_palabra(ruta,ram2)
+    escribir_excel(lista_final,camposram2,nomArchivo,workbook) # Impresion en archivo de excel
+    lista_final = buscar_palabra(ruta,disco)
+    escribir_excel(lista_final,camposdisco,nomArchivo,workbook) # Impresion en archivo de excel
+    lista_final = buscar_palabra(ruta,cd)
+    escribir_excel(lista_final,camposcd,nomArchivo,workbook) # Impresion en archivo de excel
+    lista_final = buscar_palabra(ruta,monitor)
+    escribir_excel(lista_final,camposmonitor,nomArchivo,workbook) # Impresion en archivo de excel
+    lista_final = buscar_palabra(ruta,red)
+    escribir_excel(lista_final,camposred,nomArchivo,workbook) # Impresion en archivo de excel
+    lista_final = buscar_palabra(ruta,sistema) 
+    escribir_excel(lista_final,campossistema,nomArchivo,workbook) # Impresion en archivo de excel
+print("Proceso terminado")
 
 
 
--- PROGRAMA/prueba.py
+++ PROGRAMA/prueba.py
@@ -1,4 +1,14 @@
-ruta = "COMUNICACIONES2.CSV"
-ruta = ruta[:-4]
-ruta = ruta + ".xlms"
-print(ruta)
+import psutil
+import platform
+from datetime import datetime
+
+def get_size(bytes, suffix="B"):
+    factor = 1024
+    for unit in ["","K","M","G","T","P"]: 
+        if bytes < factor:
+            return f"{bytes:.2f}{unit}{suffix}"
+        bytes /= factor
+
+uname = platform.uname()
+# uname.node = Nombre del equipo
+print(f"system:{psutil.cpu_count(logical=True)}")
